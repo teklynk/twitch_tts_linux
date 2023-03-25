15856de923d88b581291c14259d6e26f179686e3
teklynk <teklynk@protonmail.com>
2023-03-25 17:50:04 -0400

    added modsOnly option refactoring

--- index.js
+++ index.js
@@ -1,18 +1,67 @@
 const tmi = require('tmi.js');
 const fs = require('fs');
+
 const ttsFile = './tts.txt';
+const modsOnly = false;
+const channelName = 'teklynk';
+const ttsCommand = '!tts';
 
 const client = new tmi.Client({
-	channels: ['teklynk']
+
+	channels: [channelName]
+
 });
 
 client.connect();
 
-client.on('message', (channel, tags, message, self) => {
-	if (message.startsWith("!tts ")) {
-	    console.log(`${tags['display-name']}: ${message}`);
-        message = message.substring(4);
-        message = message.trim();
-        fs.writeFileSync(ttsFile, message);
+client.on('message', (channel, user, message, self) => {
+
+    // Ignore echoed messages
+    if (self) {
+
+        return false;
+
     }
+
+    if (user['message-type'] === 'chat' && message.startsWith(ttsCommand + " ")) {
+
+        if (modsOnly === true && (user.mod || user.username.toLowerCase() === channelName.toLowerCase().trim())) {
+
+            writeMessage(ttsFile, message, true);
+
+        } else if (modsOnly === false || user.username.toLowerCase() === channelName.toLowerCase().trim()) {
+
+            writeMessage(ttsFile, message, true);
+
+        }
+
+    }
+
 });
+
+function writeMessage(file, chatmessage, blocking) {
+
+    chatmessage = chatmessage.substring(ttsCommand.length).trim();
+
+    // If True, don't write to file if file already has contents.
+    if (blocking === true) {
+
+        let readFile = fs.readFileSync(file).toString();
+
+        if (readFile.length > 0) {
+
+            return false;
+
+        } else {
+
+            fs.writeFileSync(file, chatmessage);
+
+        }
+
+    } else {
+
+        fs.writeFileSync(file, chatmessage);
+
+    }
+
+}

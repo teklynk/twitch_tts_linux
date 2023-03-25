5eaea7d3be9d0af73cfa73c6073c8133f8cf2cdf
teklynk <ryanjones153@gmail.com>
2023-03-25 00:54:55 -0400

    fixed issues with md5 file check. added example for say command

--- ttsService.sh
+++ ttsService.sh
@@ -2,30 +2,35 @@
 
 filename="$1"
 
-# clear tts.txt file
+# clear tts.txt file on start
 truncate -s 0 "$filename"
+
+m1=$(md5sum "$filename")
 
 # Checks if md5sum of the file has changed. Much better than using modified date.
 while true
 do
-
-  m1=$(md5sum "$filename")
 
   m2=$(md5sum "$filename")
 
   if [ "$m1" != "$m2" ] ; then
 
     # read contents of tts.txt
-    textValue=`cat tts.txt`
+    textValue=`cat "$filename"`
 
-    # use spd-say/speech-dispatch to say the message from the tts.txt file
-    spd-say --voice-type male2 --pitch -10 --rate -40 --punctuation-mode some --wait "$textValue"
+    # Option 1
+    # apt install speech-dispatcher
+    spd-say --voice-type male2 --pitch -10 --rate -50 --punctuation-mode some --wait "$textValue"
 
-    # clear tts.txt file
+    # Option 2
+    # apt install gnustep-gui-runtime
+    #say -f "$filename"
+
+    # clear tts.txt file after reading the text
     truncate -s 0 "$filename"
 
-    # cool down time
-    sleep 10
+    # wait before reading the next tts message
+    sleep 3
 
   fi
 
